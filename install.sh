6fe54089b08916a673785185d97697a33841651f
Carlos Castellanos <ccverak@gmail.com>
2019-10-24 22:58:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 unlink ~/.gitconfig
+unlink ~/.vimrc
 unlink ~/.zshrc
 unlink ~/.fzf
 
 ln -s "$DIR/gitconfig" ~/.gitconfig
 ln -s "$DIR/zshrc" ~/.zshrc
+ln -s "$DIR/vimrc" ~/.vimrc
 ln -s "$DIR/fzf" ~/.fzf
 
 git submodule init
