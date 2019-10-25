2030f45f7bbce1f5c328a2749566c3aa579ade3d
Carlos Castellanos <ccverak@gmail.com>
2019-10-25 17:05:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ unlink ~/.gitconfig
 unlink ~/.vimrc
 unlink ~/.zshrc
 unlink ~/.fzf
+unlink ~/.tmux.conf
 
 ln -s "$DIR/gitconfig" ~/.gitconfig
 ln -s "$DIR/zshrc" ~/.zshrc
 ln -s "$DIR/vimrc" ~/.vimrc
 ln -s "$DIR/fzf" ~/.fzf
+ln -s "$DIR/tmux.conf" ~/.tmux.conf
 
 git submodule init
 git submodule update
