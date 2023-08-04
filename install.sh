01f4761adcca1113fd5e9efc0d65dcaf1df5185c
Carlos Castellanos <ccverak@gmail.com>
2023-08-04 19:00:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -85,6 +85,11 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
   asdf install nodejs 14.16.0
   asdf global nodejs 14.16.0
+
+
+  source ~/.zshrc
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 
