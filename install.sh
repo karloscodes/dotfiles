0ab49d57efa40b9a00cd5705c25a16c0ee27f456
Carlos Castellanos <me@karloscodes.com>
2024-07-24 16:59:13 +0200

    Updates install.sh

--- install.sh
+++ install.sh
@@ -79,11 +79,11 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   
-  # # Install Oh My Zsh plugins
-  # mkdir -p ~/.oh-my-zsh/custom/plugins
-  # cd ~/.oh-my-zsh/custom/plugins
-  # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-  # git clone https://github.com/zsh-users/zsh-autosuggestions.git
+  # Install Oh My Zsh plugins
+  mkdir -p ~/.oh-my-zsh/custom/plugins
+  cd ~/.oh-my-zsh/custom/plugins
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git
 
 
   brew install --cask visual-studio-code
