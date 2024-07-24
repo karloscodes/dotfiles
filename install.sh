9a1795c9c65db6819a72e1f99957949c3d8deedc
Carlos Castellanos <me@karloscodes.com>
2024-07-24 10:07:46 +0200

    Changes

--- install.sh
+++ install.sh
@@ -66,6 +66,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   sudo chown -R $USER $BREW_PREFIX/Homebrew
   sudo chown -R $USER $BREW_PREFIX/bin
 
+  # Install and Activate mise
+  curl https://mise.run | sh
+  echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+
   sudo chown -R $USER /usr/local/share/zsh /usr/local/share/zsh/site-functions
 
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
